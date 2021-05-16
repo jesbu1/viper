f8e0d29cc11e8f1b5aa57be3e990eb56d6cd86d9
Jesse Zhang <jessezhang5@gmail.com>
2021-05-15 17:36:28 -0700

    update

---
+++ python/viper/pong/custom_dqn.py
@@ -0,0 +1,102 @@
+import torch.nn as nn
+import torch
+
+import numpy as np
+
+class DQN(nn.Module):
+    def __init__(self, env, model_path=None, train=False):
+        self.env = env
+        self.model_path = model_path
+        self.num_actions = env.action_space.n
+        self.input_shape = env.observation_space.shape
+        self.batch_size=32
+        self.epsilon=0.1
+        self.num_timesteps=1e6
+        self.gamma=0.99
+        self.hidden_size=100
+        self.mem_max_size=100000
+        #q_module_list = [nn.GRUCell(input_size=self.input_shape, hidden_size=self.hidden_size, bias=True)]
+        q_module_list = [nn.Linear(in_features=self.input_shape, out_features=self.hidden_size)]
+        q_module_list.append(nn.ReLU())
+        for i in range(3):
+            q_module_list.append(nn.Linear(in_features=self.hidden_size, out_features=self.hidden_size))
+            q_module_list.append(nn.ReLU())
+        q_module_list.append(nn.Linear(in_features=self.hidden_size, out_features=self.num_actions))
+        self.q = nn.Sequential(*q_module_list)
+        if model_path and not train:
+            self.load_state_dict(torch.load(model_path))
+        self.model_path = model_path
+        self.gru_hidden_state = torch.zeros((self.batch_size, self.hidden_size))
+        self.replay_memory=[]
+        self.optimizer = torch.optim.Adam(self.q.parameters())
+
+    def reset_hidden(self):
+        self.gru_hidden_state = torch.zeros((self.batch_size, self.hidden_size))
+
+    def replay(self):
+        # choose <s,a,r,s',done> experiences randomly from the memory
+        minibatch = np.random.choice(self.replay_memory, self.batch_size, replace=True)
+        # create one list containing s, one list containing a, etc
+        s_l =      np.array(list(map(lambda x: x['s'], minibatch)))
+        a_l =      np.array(list(map(lambda x: x['a'], minibatch)))
+        r_l =      np.array(list(map(lambda x: x['r'], minibatch)))
+        sprime_l = np.array(list(map(lambda x: x['sprime'], minibatch)))
+        done_l   = np.array(list(map(lambda x: x['done'], minibatch)))
+        # Find q(s', a') for all possible actions a'. Store in list
+        # We'll use the maximum of these values for q-update  
+        qvals_sprime_l = self.predict_q(sprime_l)
+        # Find q(s,a) for all possible actions a. Store in list
+        target_f = self.predict_q(s_l)
+        # q-update target
+        # For the action we took, use the q-update value  
+        # For other actions, use the current nnet predicted value
+        for i,(s,a,r,qvals_sprime, done) in enumerate(zip(s_l,a_l,r_l,qvals_sprime_l, done_l)): 
+            if not done:  
+                target = r + self.gamma * np.max(qvals_sprime)
+            else:         
+                target = r
+            target_f[i][a] = target
+        # Update weights of neural network with fit() 
+        # Loss function is 0 for actions we didn't take
+        self.train(s_l, target_f, epochs=1, verbose=0)
+
+    def interact(self):
+        timesteps = 0
+        while timesteps < self.num_timesteps:
+            s = self.env.reset()
+            done=False
+            r_sum = 0
+            while not done: 
+                # Uncomment this to see the agent learning
+                # env.render()
+                
+                # Feedforward pass for current state to get predicted q-values for all actions 
+                qvals_s = model.predict(s.reshape(1,4))
+                # Choose action to be epsilon-greedy
+                if np.random.random() < self.epsilon:  
+                    a = self.env.action_space.sample()
+                else:                             
+                    a = np.argmax(qvals_s); 
+                # Take step, store results 
+                sprime, r, done, info = self.env.step(a)
+                r_sum += r 
+                # add to memory, respecting memory buffer limit 
+                if len(self.replay_memory) > self.mem_max_size:
+                    self.replay_memory.pop(0)
+                self.replay_memory.append({"s":s,"a":a,"r":r,"sprime":sprime,"done":done})
+                # Update state
+                s=sprime
+                # Train the nnet that approximates q(s,a), using the replay memory
+                model=self.replay()
+                # Decrease epsilon until we hit a target threshold 
+                if self.epsilon > 0.01:      
+                    self.epsilon -= 0.001
+            print("Total reward:", r_sum)
+        torch.save(self.state_dict(), self.model_path)
+
+    def predict_q(self, obs):
+        return self.q(obs)
+
+    def predict(self, obs):
+        acts = torch.argmax(self.q(obs), -1)
+        return acts
---
+++ python/viper/pong/train_dqn_karel.py
@@ -0,0 +1,82 @@
+# Copyright 2017-2018 MIT
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from os import environ
+from karel_env.karel_gym_env import KarelGymEnv
+from ..core.rl import *
+from .karel import *
+from .dqn import *
+from ..core.dt import *
+from ..util.log import *
+from collections import Iterable
+from .train_dqn_karel import DQN
+import random
+
+environments = [
+                'cleanHouse',
+                'fourCorners',
+                'harvester',
+                'randomMaze',
+                'stairClimber_sparse',
+                'topOff',
+                ]
+env_to_hw = dict(
+    cleanHouse=(14, 22),
+    fourCorners=(12, 12),
+    harvester=(8, 8),
+    randomMaze=(8, 8),
+    stairClimber_sparse=(12, 12),
+    topOff=(12, 12),
+)
+env_to_time = dict(
+    cleanHouse=300,
+    fourCorners=100,
+    harvester=100,
+    randomMaze=100,
+    stairClimber_sparse=100,
+    topOff=100,
+)
+
+class AttrDict(dict):
+    def __init__(self, *args, **kwargs):
+        super(AttrDict, self).__init__(*args, **kwargs)
+        self.__dict__ = self
+
+def learn_q(input_args):
+    # Parameters
+    env_task = input_args.env_task
+    args = dict(task_definition='custom_reward',
+                env_task=env_task,
+                max_episode_steps=100,
+                obv_type='local',
+                wall_prob=0.25,
+                height=env_to_hw[env_task][0],
+                width=env_to_hw[env_task][1],
+                incorrect_marker_penalty=True,
+                delayed_reward=True,
+                seed=random.randint(0, 100000000))
+    config = AttrDict()
+    config.update(args) 
+    env = KarelGymEnv(config)
+    env._max_episode_steps = config.max_episode_steps
+    os.mkdir(f"../data/saved_dqn/karel/{env_task}")
+    model_path = f'../data/saved_dqn/karel/{env_task}/saved'
+    q_func = DQN(env=env, model_path=model_path, train=True)
+    q_func.interact()
+    
+
+if __name__ == '__main__':
+    input_args = AttrDict()
+    input_args.env_task = environments[0]
+    learn_q(input_args)
