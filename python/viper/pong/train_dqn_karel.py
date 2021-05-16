70de5772fa6a123cc375acc6e9ce87d2202cd508
Jesse Zhang <jessezhang5@gmail.com>
2021-05-16 16:13:30 -0700

    update

--- python/viper/pong/train_dqn_karel.py
+++ python/viper/pong/train_dqn_karel.py
@@ -79,5 +79,5 @@ def learn_q(input_args):
 
 if __name__ == '__main__':
     input_args = AttrDict()
-    input_args.env_task = "randomMaze"
+    input_args.env_task = "harvester"
     learn_q(input_args)
